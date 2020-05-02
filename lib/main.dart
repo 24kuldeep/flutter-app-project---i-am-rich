e001d1edb58503d1c3e1f0eeda57b927f9ba5dda
= <=>
2020-05-02 23:37:45 +0530

    [assets add kiya]

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[900],
+        backgroundColor: Colors.blueGrey[800],
         appBar: AppBar(
           centerTitle: true,
           title: Text("I Am Rich"),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Image(
-            image: NetworkImage(
-              'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
-            ),
+            image: AssetImage("images/i_am_rich_app_icon.png"),
           ),
       ),
     );
